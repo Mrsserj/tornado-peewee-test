f90e9ab309eba634bd81b1bde6416e34cba0d402
Sergey Kuzmenko <kuzmenko75@gmail.com>
2020-03-12 10:17:02 +0800

    add favicon

--- application.py
+++ application.py
@@ -13,7 +13,7 @@ application = tornado.web.Application(
     handlers=url,
     template_path="templates",
     login_url=login_url,
-    cookie_secret="sdfsd7f68sf6sdfd8f7f6s8df68sl",
+    cookie_secret="sdfsd7f68sf6sdfd8f7f6s8df68sl"
 )
 
 application.objects = Manager(db, loop=loop)
--- routes.py
+++ routes.py
@@ -1,6 +1,6 @@
 # coding=utf-8
 
-
+from tornado.web import StaticFileHandler
 from handlers import TestListHandler, CreateUserHandler, UserLoginHandler, UserLogoutHandler, QuestionHandler, \
     PageNotFoundHandler, ResultHandler
 
@@ -11,5 +11,6 @@ url = [
     (r'/logout', UserLogoutHandler),
     (r'/quest', QuestionHandler),
     (r'/result', ResultHandler),
+    (r'/(favicon.ico)', StaticFileHandler, {"path": "static"}),
     (r'.*', PageNotFoundHandler),
 ]
